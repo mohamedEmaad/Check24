d19d94af761af01dc836a6108342b98dfffde9e4
Mohamed Emad <mohammed.emaad94@gmail.com>
2021-11-23 09:46:20 +0200

    Initial Commit

---
+++ Check24Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Check24Test
+//
+//  Created by Mohamed Emad on 11/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
